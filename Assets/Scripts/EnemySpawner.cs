06d277f7423cb80bf3b74ef4b537ce7bd9c3c947
Emil Jonsson <emilbojonsson@gmail.com>
2021-03-18 17:22:10 +0100

    Add endless background, WIP shader

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -16,6 +16,7 @@ public class EnemySpawner : MonoBehaviour
     void Start()
     {
         stage = 0;
+        SpawnEnemy();
     }
 
     // Update is called once per frame
@@ -27,10 +28,7 @@ public class EnemySpawner : MonoBehaviour
             if (transform.childCount < enemyLimit)
             {
                 timer = 0;
-                float angle = Random.Range(0, 360);
-                float distance = Random.Range(minrange, maxrange);
-                Vector3 randomDir = Quaternion.Euler(0f, 0f, Random.Range(0, 360)) * Vector3.right * distance;
-                Instantiate(EnemyPrefab[Random.Range(0, stage + 1)], player.transform.position + randomDir, new Quaternion(0, 0, 0, 0), this.transform);
+                SpawnEnemy();
             }
         }
     }
@@ -38,5 +36,13 @@ public class EnemySpawner : MonoBehaviour
     public void AdvanceStage()
     {
         stage++;
+    }
+
+    private void SpawnEnemy()
+    {
+        float angle = Random.Range(0, 360);
+        float distance = Random.Range(minrange, maxrange);
+        Vector3 randomDir = Quaternion.Euler(0f, 0f, Random.Range(0, 360)) * Vector3.right * distance;
+        Instantiate(EnemyPrefab[Random.Range(0, stage + 1)], player.transform.position + randomDir, new Quaternion(0, 0, 0, 0), this.transform);
     }
 }
---
+++ Assets/Scripts/MoveBackground.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBackground : MonoBehaviour
+{
+    public GameObject player;
+    public float size = 100.0f;
+    private float timer;
+    public float updateTime;
+    private Color BackgroundColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer > updateTime)
+        {
+            updateBackgroundPosition();
+            timer = 0;
+        }
+    }
+
+    void updateBackgroundPosition()
+    {
+        float playerX = player.transform.position.x;
+        float playerY = player.transform.position.y;
+
+        if(playerX - transform.position.x > (size / 2))
+        {
+            transform.Translate(new Vector3(size, 0, 0));
+            UpdateColor();
+        } else if (transform.position.x - playerX > (size / 2))
+        {
+            transform.Translate(new Vector3(-size, 0, 0));
+        }
+
+        if (playerY - transform.position.y > (size / 2))
+        {
+            transform.Translate(new Vector3(0, size, 0));
+        }
+        else if (transform.position.y - playerY > (size / 2))
+        {
+            transform.Translate(new Vector3(0, -size, 0));
+        }
+
+    }
+
+    void UpdateColor()
+    {
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            GameObject child = transform.GetChild(0).gameObject;
+            //Get the Renderer component from the new cube
+            var renderer = child.GetComponent<Renderer>();
+
+            //Call SetColor using the shader property name "_Color" and setting the color to red
+            renderer.material.SetColor("_Color", Color.red);
+        }
+    }
+}
